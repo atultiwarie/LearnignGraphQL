33b36872252bcfa1a7707d2eb3801af6e2a5ce37
atultiwarie <atultiwari4767@gmail.com>
2025-08-26 23:56:11 +0530

    updated backend logic

--- app.js
+++ app.js
@@ -1,15 +1,62 @@
-require('dotenv').config()
-const express= require('express')
-const PORT = process.env.PORT || 3000
-const {graphqlHTTP} = require('express-graphql')
-const schema = require('./Schema/schema')
-const app = express()
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const axios = require("axios");
 
-app.use('/graphql', graphqlHTTP({
-    schema,
-    graphiql: process.env.NODE_ENV === 'development'
-}))
+async function startServer() {
+  const server = new ApolloServer({
+    typeDefs: `
+            type User {
+                id:ID!
+                name:String!
+                username:String!
+                email:String!
+                phone:String!
+                website:String!
+            }
 
-app.listen(PORT,()=>{
-    console.log(`Server is running on port http://localhost:${PORT}`)
-})
+            type Todo {
+                id: ID!
+                title: String!
+                completed: Boolean!
+                user:User!
+            }
+
+            type Query {
+                getTodos: [Todo]
+                getAllUsers:[User]
+                getUser(id:ID!):User
+            }
+        `,
+    resolvers: {
+      Todo: {
+        user: async (todo) =>
+          await axios
+            .get(`https://jsonplaceholder.typicode.com/users/${todo.userId}`)
+            .then((res) => res.data),
+      },
+      Query: {
+        getTodos: async () =>
+          await axios
+            .get("https://jsonplaceholder.typicode.com/todos")
+            .then((res) => res.data),
+        getAllUsers: async () =>
+          await axios
+            .get("https://jsonplaceholder.typicode.com/users")
+            .then((res) => res.data),
+        getUser: async (parent, args) =>
+          await axios
+            .get(`https://jsonplaceholder.typicode.com/users/${args.id}`)
+            .then((res) => res.data),
+      },
+    },
+  });
+
+  // Use standalone server instead of Express handler
+  const { url } = await startStandaloneServer(server, {
+    listen: { port: 8000 },
+  });
+
+  console.log(`Server running at ${url}`);
+}
+
+startServer();
